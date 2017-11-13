d581936da0d902e6bc3ea583f57383db28d1260c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-11-13 16:37:00 +0100

    more exercises about python

---
+++ python/thehardway/e05.py
@@ -0,0 +1,15 @@
+my_name = 'Maria L'
+my_age = 36
+my_height = 72 # inches
+my_weight = 145 # pounds
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %s." % my_name)
+print("She's %d inches tall." % my_height)
+print("She's %d pounds heavy." % my_weight)
+print("Actually that's not too heavy.")
+print("She's got %s eyes and %s hair" % (my_eyes, my_hair))
+
+print("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
