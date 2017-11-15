8dfd3188b346cccbd0f590db9fd69a88e4a43869
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-11-15 13:39:24 +0100

    test finished

--- python/thehardway/test25.py
+++ python/thehardway/test25.py
@@ -10,3 +10,7 @@ words
 e25.print_first_word(sorted_words)
 e25.print_last_word(sorted_words)
 sorted_words
+sorted_words = e25.sort_sentence(sentence)
+sorted_words
+e25.print_first_and_last(sentence)
+e25.print_first_and_last_sorted(sentence)
