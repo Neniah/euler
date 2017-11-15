dcc2dd86cdd948628913aac312d7e7e5fd06da46
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-11-15 13:19:38 +0100

    strings and imports

---
+++ python/thehardway/test25.py
@@ -0,0 +1,12 @@
+import e25
+sentence = "All good things come to those who wait."
+words = e25.break_words(sentence)
+words
+sorted_words = e25.sort_words(words)
+sorted_words
+e25.print_first_word(words)
+e25.print_last_word(words)
+words
+e25.print_first_word(sorted_words)
+e25.print_last_word(sorted_words)
+sorted_words
